abdc7c695c6f132c8ee8606193c7e3f2b73fbd76
Pravin-Sonwane-2004 <pravinson222@gmail.com>
2026-02-04 13:37:15 +0530

    now this is main so far

--- Array/Q1FrequencyOfElements.java
+++ Array/Q1FrequencyOfElements.java
@@ -11,7 +11,8 @@
 
 class Q1FrequencyOfElements {
 	void main(){
-		int [] arr = {1, 2, 2, 3, 3, 3, 4};
+		int [] arr
+		 = {1, 2, 2, 3, 3, 3, 4};
 		boolean [] visited = new boolean[arr.length];
 		for(int i = 0;i<arr.length;i++) {
 			int count = 1;
