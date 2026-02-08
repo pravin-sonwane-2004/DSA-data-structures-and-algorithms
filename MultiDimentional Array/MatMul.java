f521e935f818cc91dfcd976b58a64b438c802b0f
Pravin-Sonwane-2004 <pravinson222@gmail.com>
2026-02-08 09:18:57 +0530

    learning the window

---
+++ MultiDimentional Array/MatMul.java
@@ -0,0 +1,17 @@
+public class MatMul {
+    int[][] firstMat = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+    };
+    int[][] secMat = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+    };
+
+    int [][] thirdMat = new int[firstMat.length] [firstMat.length];
+
+    for(int i=0;i<firstMat)
+
+}
