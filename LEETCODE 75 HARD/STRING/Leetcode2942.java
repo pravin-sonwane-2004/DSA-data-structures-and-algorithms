c3438f952dd98c214981766a55400176767d2c1c
Pravin-Sonwane-2004 <pravinson222@gmail.com>
2026-02-09 09:48:26 +0530

    need to understand how this work     for(int i=0;i<words.length;i++) {             if(words[i].indexOf(x)!= -1) {                 list.add(i);             }

---
+++ LEETCODE 75 HARD/STRING/Leetcode2942.java
@@ -0,0 +1,48 @@
+package STRING;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 2942. Find Words Containing Character Easy Topics premium lock icon Companies
+//  Hint You are given a 0-indexed array of strings words and a character x.
+
+// Return an array of indices representing the words that contain the character x.
+
+// Note that the returned array may be in any order.
+
+// Example 1:
+// Input:words=["leet","code"],
+// x="e"Output:[0,1]Explanation:"e"occurs in both words:"leet",
+// and"code".Hence,we return indices 0 and 1.
+
+//  Example 2:
+// Input:words=["abc","bcd","aaaa","cbc"],
+// x="a"Output:[0,2]Explanation:"a"occurs in"abc",
+// and"aaaa".Hence,we return indices 0 and 2. 
+
+//Example 3:
+// Input:words=["abc","bcd","aaaa","cbc"],x="z"
+// Output:[]Explanation:"z"does not occur in any of the words.Hence,we return an empty array.
+
+//neeed to understanc flow of program and why this is happening
+// for(int i=0;i<words.length;i++){if(words[i].indexOf(x)!=-1){list.add(i);}
+
+public class Leetcode2942 {
+    public static List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<words.length;i++) {
+            if(words[i].indexOf(x)!= -1) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+    void main() {
+        String [] words={"abc","bcd","aaaa","cbc"};
+        char x = 'a';
+        IO.println(findWordsContaining(words, x));
+    }
+
+    
+}
