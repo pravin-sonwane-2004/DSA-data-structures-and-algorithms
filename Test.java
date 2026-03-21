09ad26ff89005e86917f012f158a04f2a273c4a1
Pravin-Sonwane-2004 <pravinson222@gmail.com>
2026-03-21 13:50:06 +0530

    Added Some Things

--- Test.java
+++ Test.java
@@ -239,3 +239,52 @@
 
 
 
+
+
+
+// // Question 1
+// This vacation you went to visit the golden house. There are N rooms in this golden
+// house and its owner needs someone to take care of the management of this house.
+// As you have been unemployed for a long time, you are interested in this job. Owner
+// of this house wanted an intelligent manager for this role, so he created one puzzle
+// within that golden house. The person who will be able to solve that puzzle will be the
+// manager of the golden house.
+// Owner of the house kept some numbers of golden coins in each room. You have to
+// choose two rooms, one from where you will enter and other one from where you will
+// exit. From any room either you can exit, or you can move to the next room. While
+// visiting any room you will collect all the gold coins, and if you enter any room then
+// you can’t skip collecting gold coins from that room, you have to take those coins.
+// Owner wants to have exactly K coins, when you exit the room, he guarantees that
+// there will be at least one possible solution for this puzzle.
+// Given number of rooms (N), and number of gold coins in N rooms. You have to find
+// room numbers from where you will start and from where you will exit. If there are
+// more than one solution possible, then you have to provide a solution with a smaller
+// starting room number.
+// You can assume that room numbers will start from 1 and end at N.
+// Hint: Find a continuous subsequence whose sum will be exactly equal to K.
+// Example 1:
+// • Input:
+// 10 15 -> N =10, K = 15
+// 5 3 7 14 18 1 18 4 8 3 -> Number of gold coins in each room.
+// • Output:
+// 1 3
+
+class VaicationToGoldenHouse {
+	void main() {
+		int arr [] = {5 ,3 ,7 ,14, 18, 1 ,18 ,4 ,8 ,3};
+		int n = 10;
+		int k= 15;
+		int currSum = 0;
+		int left = 0;
+		for(int i=0;i<arr.length-1;i++) {
+			currSum += arr[i];
+			while(currSum>k) {
+				currSum -= arr[left];
+				left++;
+			}
+			if(currSum == k) {
+				IO.println(left+1 + "   "+ (i+1));
+			}
+		}
+	}                                
+}
