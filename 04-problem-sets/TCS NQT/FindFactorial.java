f743381e046893aeb96b72edecef0ff6428f16eb
Pravin-Sonwane-2004 <pravinson222@gmail.com>
2026-05-16 08:10:25 +0530

    feat: solving sove easy questions everyday

--- 04-problem-sets/TCS NQT/FindFactorial.java
+++ 04-problem-sets/TCS NQT/FindFactorial.java
@@ -1,14 +1,27 @@
 public class FindFactorial {
     void main() {
-      for(int i=0;i<1000;i++) {
+      for(int i=0;i<10;i++) {
         IO.println(i+ "->"+findFactorial(i)); // integer overflow we need to use here Long 
       }
     }
-    int findFactorial(int n) {
-        int factorial = 1;
-        for(int i=n;i>0;i--) {
-            factorial *= i;
-        }
-        return factorial;
+    // int findFactorial(int n) {
+    //     int factorial = 1;
+    //     for(int i=n;i>0;i--) {
+    //         factorial *= i;
+    //     }
+    //     return factorial;
+    // }
+    long findFactorial(int n) {
+    if (n < 0) {
+        throw new IllegalArgumentException("Factorial is not defined for negative numbers");
     }
+
+    long factorial = 1;
+
+    for (int i = n; i > 0; i--) {
+        factorial *= i;
+    }
+
+    return factorial;
+}
 }
