abd63072dc7ff0d2ac131f890ec7c363439c2153
pravin-sonwane-2004 <171813375+pravin-sonwane-2004@users.noreply.github.com>
2026-09-12 12:00:00 +0530

    Your commit message

--- CORE/Class-Object-Implementation/New folder (2)/pojo.java
+++ CORE/Class-Object-Implementation/New folder (2)/pojo.java
@@ -23,25 +23,9 @@ class pojo {
 		
 	}
 }
-$env:GIT_AUTHOR_DATE="2026-09-01 14:30:00"
-$env:GIT_COMMITTER_DATE="2026-09-01 14:30:00"
 
-git add .
-git commit -m "Updated application functionality"$env:GIT_AUTHOR_DATE="2026-09-01 14:30:00"
-$env:GIT_COMMITTER_DATE="2026-09-01 14:30:00"
 
-git add .
-git commit -m "Updated application functionality"$env:GIT_AUTHOR_DATE="2026-09-01 14:30:00"
-$env:GIT_COMMITTER_DATE="2026-09-01 14:30:00"
 
-git add .
-git commit -m "Updated application functionality"
-
-class pojo {
-	void main() {
-		
-	}
-}
 
 class pojo {
 	void main() {
